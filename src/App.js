95d7de73f305ff19991a4b7a26ba4908a6383ac3
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-04-06 16:16:29 +0200

    par state from text input

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
 import Golfcourse from './components/Golfcourse';
+import { useState } from 'react';
 
 function App() {
+  const [par, setPar] = useState('72');
   const showGolfcourses = true;
   return (
     <div className="App">
       {showGolfcourses ? (
         <>
-          <Golfcourse />
-          <Golfcourse />
-          <Golfcourse />
-          <Golfcourse />
-          <Golfcourse />
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setPar(e.target.value);
+            }}
+          />
+          <Golfcourse name="Golf Club de Perigueux" par="72" />
+          <Golfcourse name="Golf de la Marterie" par={par} />
+          <Golfcourse name="Epherra" />
         </>
       ) : (
         <p> You cannot see golf courses </p>
--- src/components/Golfcourse.js
+++ src/components/Golfcourse.js
@@ -1,5 +1,18 @@
-function Golfcourse() {
-  return <h3>Here is a golf course </h3>;
+function Golfcourse(props) {
+  return (
+    <>
+      <h3>Golf course {props.name} </h3>
+      {/*Simpler but style will be the same on both response*/}
+      <p>{props.par ? props.par : 'no par infos'}</p>
+
+      {/*More complexe but flexier*/}
+      {/* {props.par ? (
+        <p class="par">props.par</p>
+      ) : (
+        <p class="noPar">no par infos</p>
+      )} */}
+    </>
+  );
 }
 
 export default Golfcourse;
